f0a70c6d49b4ed28215a159ab98aa767df7cbbb7
khuzaimaahmed1998 <129598994+khuzaimaahmed1998@users.noreply.github.com>
2023-10-05 20:45:29 +0500

    php assignments

--- marksheet.php
+++ marksheet.php
@@ -4,90 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        h1{
-            text-align: center;
-            font-size: 60;
-        }
-        table {
-            border-collapse: collapse;
-            margin: 20px auto;            
-            width: 60%;
-        }
-        th, td{
-            height: 40px;
-            border : solid black thin ;
-        }
-        th{
-            width: 5%;
-            text-align: center;
-        }
-        td{
-            width: 10%;
-            text-align: center;
-        }
-
-    </style>
 </head>
 <body>
 <?php
-    // Student Information
-    $rn = 6558;
-    $sn = "Shahid";
-    $class = 9;
 
-    // Subject Scores
+
+    echo "<h1>Marksheet</h1>"
+    $sno = 54;
+    $name = "Khuzaima";
+    $class = 9;
     $eng = 94;
-    $urdu = 47;
-    $islamiyat = 89;
-    $ps = 32;
-    $maths = 2;
+    $urdu = 100;
+    $maths = 89;
     $science = 71;
 
     // Calculating Obtained Marks and Percentage
-    $obt_marks = $eng + $urdu + $islamiyat + $ps + $maths + $science;
-    $total_marks = 600;
-    $percentage = ($obt_marks / $total_marks) * 100;
+    $marks = $eng + $urdu + $maths + $science;
+    $total_marks = 400;
+    $per = ($marks / $total_marks) * 100;
     $grade;
-    $remarks;
 
-    // Determines Grades and Remarks
     if ($percentage >= 80) {
         $grade = 'A+';
-        $remarks = 'Excellent';
-    }else if ($percentage >= 70 && $percentage <= 79) {
+    }else if ($per>=70 && $per<=79) {
         $grade = 'A';
-        $remarks = 'Very Good';
-    }else if ($percentage >= 60 && $percentage <= 69) {
+    }else if ($per>=60 && $per<=69) {
         $grade = 'B';
-        $remarks = 'Good';
-    }else if ($percentage >= 50 && $percentage <= 59) {
+    }else if ($per>= 50 && $per<=59) {
         $grade = 'C';
-        $remarks = 'Average';
-    }else if ($percentage >= 40 && $percentage <= 49) {
+    }else if ($per>= 40 && $per<= 49) {
         $grade = 'D';
-        $remarks = 'Satisfactory';
-    }else if ($percentage >= 0 && $percentage <= 39) {
-        $grade = 'F';
-        $remarks = 'FAIL';
+    }else if ($per>= 0 && $per<=39) {
+        $grade = 'FAIL';
     }
+        echo "<b>SNO:</b>" . $sno . "<br>";
+        echo "<b>Name:</b>" . $name . "<br>";
+        echo "<b>Class:</b>" . $class . "<br>";
+        echo "<b>English:</b>" . $eng . "<br>";
+        echo "<b>Urdu:</b>" . $urdu . "<br>";
+        echo "<b>Maths:</b>" . $maths . "<br>";
+        echo "<b>Science:</b>" . $science . "<br>";
+        echo "<b>Marks:</b>" . $marks . "<br>";
+        echo "<b>Percentage:</b>" . $per . "<br>";
+        echo "<b>Grade:</b>" . $grade . "<br>";
     ?>
 
-    <h1>Marksheet</h1>
-    <table border="1">
-        <tr><th>Roll Number:</th> <td><?php echo $rn ?></td></tr>
-        <tr><th>Student Name:</th> <td><?php echo $sn ?></td></tr>
-        <tr><th>Class:</th> <td><?php echo $class ?></td></tr>
-        <tr><th>English:</th> <td><?php echo $eng ?></td></tr>
-        <tr><th>Urdu:</th> <td><?php echo $urdu ?></td></tr>
-        <tr><th>Islamiyat:</th> <td><?php echo $islamiyat ?></td></tr>
-        <tr><th>Pakistan Studies:</th> <td><?php echo $ps ?></td></tr>
-        <tr><th>Maths:</th> <td><?php echo $maths ?></td></tr>
-        <tr><th>Science:</th> <td><?php echo $science ?></td></tr>
-        <tr><th>Obtained Marks:</th> <td><?php echo $obt_marks ?> / <?php echo $total_marks ?></td></tr>
-        <tr><th>Percentage:</th> <td><?php echo $percentage ?></td></tr>
-        <tr><th>Grade:</th> <td><?php echo $grade ?></td></tr>
-        <tr><th>Remarks:</th> <td><?php echo $remarks ?></td></tr>
-    </table>
 </body>
 </html>
