936375136455ac6cccfc57d5a387b1eb72365b53
khuzaimahmed1998 <khuzaimaahmed19@gmail.com>
2023-10-05 15:38:42 +0500

    PHP Assignments

---
+++ Numberanalys.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "<h1>Number Analysis</h1>";
+    $Num = 9;
+
+    if($Num > 0){
+
+    echo $Num . " is Positive Number <br>";
+
+    }else
+
+    echo $Num . " is Negative Number <br>";
+    
+    if($Num %2==0){
+
+    echo $Num . " is Even Number <br>";
+
+    }else
+
+    echo $Num . " is Odd Number <br>";
+
+    ?>
+</body>
+</html>
---
+++ leapyear.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "<h1>Leap Year Program</h1>";
+    $Year = 2003;
+    if($Year %4==0){
+        echo $Year . " is Leap Year <br>";
+    }else 
+        echo $Year . " is not a Leap Year <br>";
+    ?>
+</body>
+</html>
