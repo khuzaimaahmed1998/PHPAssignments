8a77f06229babca807e7244e0d58183a20d4a144
khuzaimaahmed1998 <129598994+khuzaimaahmed1998@users.noreply.github.com>
2023-11-10 15:08:38 +0500

    code updated

---
+++ filehandling/assignment.php
@@ -0,0 +1,195 @@
+<?php
+$fileone= fopen("1.txt", "r");
+
+
+if (!$fileone) {
+    die("data is failed  ");
+}
+
+echo "<h1>Seerat Un Nabi S.A.W</h1>";
+while ($o = fgets($fileone)) {
+    echo $o;
+}
+
+
+?>
+
+<?php
+$filetwo= fopen("2.txt", "r");
+
+
+if (!$filetwo) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Ayesha R.A</h3>";
+while ($t = fgets($filetwo)) {
+    echo $t;
+}
+
+?>
+
+<?php
+$filethree= fopen("3.txt", "r");
+
+
+if (!$filethree) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Kadija R.A</h3>";
+while ($h = fgets($filethree)) {
+    echo $h;
+}
+
+?>
+
+<?php
+$filefour= fopen("4.txt", "r");
+
+
+if (!$filefour) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Abu Bakr R.A</h3>";
+while ($f = fgets($filefour)) {
+    echo $f;
+}
+
+?>
+
+<?php
+$filefive= fopen("5.txt", "r");
+
+
+if (!$filefive) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Umar R.A</h3>";
+while ($i = fgets($filefive)) {
+    echo $i;
+}
+
+?>
+
+<?php
+$filesix= fopen("6.txt", "r");
+
+
+if (!$filesix) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Usman R.A</h3>";
+while ($s = fgets($filesix)) {
+    echo $s;
+}
+
+?>
+
+<?php
+$fileseven= fopen("7.txt", "r");
+
+
+if (!$fileseven) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Ali R.A</h3>";
+while ($e = fgets($fileseven)) {
+    echo $e;
+}
+
+?>
+
+<?php
+$fileeight= fopen("8.txt", "r");
+
+
+if (!$fileeight) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Abu Ubaidah R.A</h3>";
+while ($g = fgets($fileeight)) {
+    echo $g;
+}
+
+?>
+
+<?php
+$filenine= fopen("9.txt", "r");
+
+
+if (!$filenine) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Sa'd ibn Abi Waqqas R.A</h3>";
+while ($n = fgets($filenine)) {
+    echo $n;
+}
+
+?>
+
+<?php
+$fileten= fopen("10.txt", "r");
+
+
+if (!$fileten) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Abdur-Rahman R.A</h3>";
+while ($b = fgets($fileten)) {
+    echo $b;
+}
+
+?>
+
+<?php
+$fileelevel= fopen("11.txt", "r");
+
+
+if (!$fileelevel) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Zubayr R.A</h3>";
+while ($l = fgets($fileelevel)) {
+    echo $l;
+}
+
+?>
+
+<?php
+$filetwelve= fopen("12.txt", "r");
+
+
+if (!$filetwelve) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Saeed R.A</h3>";
+while ($w = fgets($filetwelve)) {
+    echo $w;
+}
+
+?>
+
+<?php
+$filethirteen= fopen("13.txt", "r");
+
+
+if (!$filethirteen) {
+    die("data is failed  ");
+}
+
+echo "<h3>Hazrat Talha R.A</h3>";
+while ($d = fgets($filethirteen)) {
+    echo $d;
+}
+
+?>
